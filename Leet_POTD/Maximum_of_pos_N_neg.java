3b29f60ad5f7a8cb8a3abacbf0a00c41c3a0d3c7
Priyadarshan-Garg <www.priyadarshan2004@gmail.com>
2025-03-12 15:10:00 +0530

    Making directory an adding it

--- Leet_POTD/Maximum_of_pos_N_neg.java
+++ Leet_POTD/Maximum_of_pos_N_neg.java
@@ -9,12 +9,5 @@ public class Maximum_of_pos_N_neg {
         return (int)Math.max((int)countNeg,(int)countPos);
     }
 
-    public static void main(String[] args) {
-        int [] nums = {-2,-1,-1,1,2,3};
-        int [] nums2 = {-3,-2,-1,0,0,1,2};
-        int [] nums3 = {5,20,66,1314};
 
-        System.out.println(myFunction(nums2));
-
-    }
 }
